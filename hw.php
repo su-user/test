c56418286665bf0b6019c7126577648435a6504c
Paul <80900103+su-user@users.noreply.github.com>
2021-03-21 17:19:38 +0300

    Hello World

---
+++ hw.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>First PHP</title>
+ </head>
+ <body>
+ <?php 
+      echo '<p>Привет, мир!</p>';
+      echo "Сегодня - ".date("d F Y")."<br>";
+      echo "Текущее время - ".date("H:i:s");
+ ?>
+ </body>
+</html>
